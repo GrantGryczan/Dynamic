900a6dd021e85c048673ce64a92d68acc6b4c7b5
Grant Gryczan <grantgryczan@gmail.com>
2018-06-21 07:59:23 -0400

    Updated build script to be more automatic

--- build/build.js
+++ build/build.js
@@ -1,11 +1,19 @@
+const fs = require("fs-extra");
 const builder = require("electron-builder");
+const package = require("../package.json");
+const rename = filename => {
+	filename = filename.pop();
+	fs.rename(filename, `dist/${package.build.productName} ${package.version} Setup${filename.slice(filename.lastIndexOf("."))}`);
+};
 if(process.platform === "win32") {
 	builder.build({
 		targets: builder.Platform.WINDOWS.createTarget("nsis", [builder.Arch.x64])
-	});
+	}).then(rename);
 } else if(process.platform === "darwin") {
 	builder.build({
 		targets: builder.Platform.OSX.createTarget()
-	});
+	}).then(rename);
 }
-// TODO: build Linux
+builder.build({
+	targets: builder.Platform.LINUX.createTarget("deb")
+}).then(rename);
