4074636f93f36266220ce9ceb8dd5dcd1bc653a0
Grant Gryczan <grantgryczan@gmail.com>
2018-07-19 14:15:40 -0400

    Add timeline ruler

--- app/js/timeline.js
+++ app/js/timeline.js
@@ -24,3 +24,41 @@ const openAsset = (asset, selected, finish) => {
 		delete proj[sel].openAsset;
 	}
 };
+const addTimeUnits = quantity => {
+	if(!quantity) {
+		quantity = 1;
+	}
+	for(let i = 0; i < quantity; i++) {
+		if(!timelineRuler.lastElementChild || timelineRuler.lastElementChild.querySelectorAll(".timeUnit").length === 5) {
+			const value = timelineRuler.lastElementChild ? timelineRuler.lastElementChild[_value] + 5 : 0;
+			const timeUnitGroup = html`
+				<div class="timeUnitGroup"><div class="label">${value}</div><div class="timeUnit"></div></div>
+			`;
+			timeUnitGroup[_value] = value;
+			timelineRuler.appendChild(timeUnitGroup);
+		} else {
+			timelineRuler.lastElementChild.appendChild(html`
+				<div class="timeUnit"></div>
+			`);
+		}
+	}
+};
+const removeTimeUnits = quantity => {
+	if(!quantity) {
+		quantity = Infinity;
+	}
+	for(let i = 0; i < quantity; i++) {
+		if(timelineRuler.lastElementChild) {
+			const timeUnitAmount = timelineRuler.lastElementChild.querySelectorAll(".timeUnit").length;
+			if(timeUnitAmount) {
+				if(timeUnitAmount === 1) {
+					timelineRuler.lastElementChild.remove();
+				} else {
+					timelineRuler.lastElementChild.lastElementChild.remove();
+				}
+			}
+		} else {
+			return;
+		}
+	}
+};
